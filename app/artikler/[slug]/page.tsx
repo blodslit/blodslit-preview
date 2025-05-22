ef2b68feec71c2594af00dfde2c316f4d829980e
blodslit <evesthe@gmail.com>
2025-05-22 23:07:23 +0200

    Fix Vercel type error with async PageProps

--- app/artikler/[slug]/page.tsx
+++ app/artikler/[slug]/page.tsx
@@ -1,6 +1,20 @@
 // app/artikler/[slug]/page.tsx
 
-export default function ArticlePage({ params }: { params: { slug: string } }) {
+import { type Metadata } from 'next'
+
+type PageProps = {
+  params: {
+    slug: string
+  }
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  return {
+    title: `Artikkel: ${params.slug}`,
+  }
+}
+
+export default async function ArticlePage({ params }: PageProps) {
   return (
     <main className="prose mx-auto p-8">
       <h1>Forhåndsvisning av artikkel: {params.slug}</h1>
