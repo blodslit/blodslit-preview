ae8c9989e2621e94d7c33ae4518933ea4daf38bc
blodslit <evesthe@gmail.com>
2025-05-22 23:21:36 +0200

    Fix for Next.js/Vercel App Router type constraints

--- app/artikler/[slug]/page.tsx
+++ app/artikler/[slug]/page.tsx
@@ -1,17 +1,15 @@
 // app/artikler/[slug]/page.tsx
 
-type PageProps = {
+type Props = {
   params: {
     slug: string
   }
 }
 
-export default function ArticlePage(props: PageProps) {
-  const { slug } = props.params
-
+export default async function ArticlePage({ params }: Props) {
   return (
     <main className="prose mx-auto p-8">
-      <h1>Forhåndsvisning av artikkel: {slug}</h1>
+      <h1>Forhåndsvisning av artikkel: {params.slug}</h1>
       <p>Her kommer innholdet fra Sanity etter hvert.</p>
     </main>
   )
